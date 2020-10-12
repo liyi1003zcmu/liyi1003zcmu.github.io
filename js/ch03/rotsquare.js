3f71c3d1a54eccdb4bede8092ba9afe87275c355
lyii <liyi@zcmu.edu.cn>
2020-10-12 14:02:49 -0400

    Create rotsquare.js

---
+++ js/ch03/rotsquare.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var theta = 0.0;
+var thetaLoc;
+
+function initRotSquare(){
+	canvas = document.getElementById( "rot-canvas" );
+	gl = WebGLUtils.setupWebGL( canvas, "experimental-webgl" );
+	if( !gl ){
+		alert( "WebGL isn't available" );
+	}
+
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+	var program = initShaders( gl, "rot-v-shader", "rot-f-shader" );
+	gl.useProgram( program );
+
+	var vertices = [
+		 0,  1,  0,
+		-1,  0,  0,
+		 1,  0,  0,
+		 0, -1,  0
+	];
+
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( vertices ), gl.STATIC_DRAW );
+
+	var vPosition = gl.getAttribLocation( program, "vPosition" );
+	gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );
+
+	thetaLoc = gl.getUniformLocation( program, "theta" );
+
+	renderSquare();
+}
+
+function renderSquare(){
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	
+	// set uniform values
+	theta += 0.1;
+	if( theta > 2 * Math.PI )
+		theta -= (2 * Math.PI);
+	
+	gl.uniform1f( thetaLoc, theta );
+
+	gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
+
+	// update and render
+	window.requestAnimFrame( renderSquare );
+}
