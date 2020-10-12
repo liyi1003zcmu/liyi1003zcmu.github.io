e1ca059d876f89c8d3cd78a1ec47bb4601391a9b
lyii <liyi@zcmu.edu.cn>
2020-10-12 14:02:47 -0400

    Create drawtrianglestrip.js

---
+++ js/ch03/drawtrianglestrip.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var maxNumTriangles = 200;
+var maxNumVertices = 3 * maxNumTriangles;
+var index = 0;
+
+var colors = [
+	0.0, 0.0, 0.0, 1.0, // black
+	1.0, 0.0, 0.0, 1.0 , // red
+	1.0, 1.0, 0.0, 1.0 , // yellow
+	0.0, 1.0, 0.0, 1.0 , // green
+	0.0, 0.0, 1.0, 1.0 , // blue
+	1.0, 0.0, 1.0, 1.0 , // magenta
+	0.0, 1.0, 1.0, 1.0  // cyan
+];
+
+function initTriangleCanvas(){
+	canvas = document.getElementById( "tri-canvas" );
+	gl = WebGLUtils.setupWebGL( canvas );
+	if( !gl ){
+		alert( "WebGL isn't available" );
+	}
+
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
+
+	canvas.addEventListener( "click", function( event ){
+		gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+		var rect = canvas.getBoundingClientRect();
+		var cx = event.clientX - rect.left;
+		var cy = event.clientY - rect.top; // offset
+		var t = glMatrix.vec2.fromValues( 2 * cx / canvas.width - 1, 2 * ( canvas.height - cy ) / canvas.height - 1 );
+		gl.bufferSubData( gl.ARRAY_BUFFER, 8 * index, new Float32Array( t ) );
+
+		gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+		var c = glMatrix.vec4.fromValues( colors[index%7*4], colors[index%7*4+1], colors[index%7*4+2], colors[index%7*4+3]);
+		gl.bufferSubData( gl.ARRAY_BUFFER, 16 * index, new Float32Array( c ) );
+		index++;
+	} );
+
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
+
+	var vBuffer = gl.createBuffer(); //position
+	gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+	gl.bufferData( gl.ARRAY_BUFFER, 8 * maxNumVertices, gl.STATIC_DRAW );
+
+	var vPosition = gl.getAttribLocation( program, "vPosition" );
+	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );
+
+	var cBuffer = gl.createBuffer(); // color
+	gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+	gl.bufferData( gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW );
+
+	var vColor = gl.getAttribLocation( program, "vColor" );
+	gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vColor );
+
+	renderTriangles();
+}
+
+function renderTriangles(){
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.drawArrays( gl.TRIANGLE_STRIP, 0, index );
+
+	window.requestAnimFrame( renderTriangles );
+}
