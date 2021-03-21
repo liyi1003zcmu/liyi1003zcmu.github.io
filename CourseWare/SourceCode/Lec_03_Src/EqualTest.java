1a8a594edaf32283c846e3fce993a905bf90fd0a
limpidmouse <69877236+liyi1003zcmu@users.noreply.github.com>
2021-03-21 21:16:12 +0800

    Create EqualTest.java

---
+++ CourseWare/SourceCode/Lec_03_Src/EqualTest.java
@@ -0,0 +1,24 @@
+public class EqualTest{
+	public static void main( String[] args ){
+		/*
+		int[] iarr1 = new int[]{ 1, 2, 3, 4 };
+		int[] iarr2 = new int[]{ 1, 2, 3, 4 };
+		int[] iarr3 = new int[4];
+		for( int i = 0; i < iarr3.length; i++)
+			iarr3[i] = iarr1[i];
+		iarr3 = iarr1;
+		System.out.println( "iarr1=" + iarr1 + ", iarr2=" + iarr2 + " iarr3=" + iarr3 );
+		System.out.println( "iarr1==iarr2 " + (iarr1==iarr2) );
+		System.out.println( "iarr1.equals(iarr2) " + iarr1.equals(iarr2) );
+		System.out.println( "iarr1==iarr3 " + (iarr1==iarr3) );
+		System.out.println( "iarr1.equals(iarr3) " + iarr1.equals(iarr3) );
+		*/
+		
+		/*
+		Integer n1 = new Integer(10);
+		Integer n2 = new Integer(10);
+		System.out.println( "n1==n2 " + (n1==n2) );
+		System.out.println( "n1.equals(n2) " + n1.equals(n2) );
+		*/
+	}
+}
