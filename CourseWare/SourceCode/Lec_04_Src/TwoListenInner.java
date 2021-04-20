d71500da8353de64141a8692d518fb6234074702
liyi1003zcmu <69877236+liyi1003zcmu@users.noreply.github.com>
2021-04-20 22:11:50 +0800

    Create TwoListenInner.java

---
+++ CourseWare/SourceCode/Lec_04_Src/TwoListenInner.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class TwoListenInner{
+	private Frame f;
+	private TextField tf;
+	
+	public static void main( String[] args ){
+		TwoListenInner that = new TwoListenInner();
+		that.go();
+	}
+	
+	public void go(){
+		f = new Frame( "Two Listeners example" );
+		f.add( "North", new Label( "Click and drag the mouse" ) );
+		
+		tf = new TextField( 30 );
+		f.add( "South", tf );
+		f.addMouseMotionListener( new MouseMotionHandler() );
+		f.addMouseListener( new MouseEventHandler() );
+		f.setSize( 300, 200 );
+		f.setVisible( true );
+	}
+	
+	public class MouseMotionHandler extends MouseMotionAdapter{
+		public void mouseDragged( MouseEvent e ){
+			String s = "Mouse dragging: X = " + e.getX() + " Y = " + e.getY();
+			tf.setText( s );
+		}
+	}
+	
+	public class MouseEventHandler extends MouseAdapter{
+		public void mouseEntered( MouseEvent e ){
+			String s = "The mouse entered";
+			tf.setText( s );
+		}
+		
+		public void mouseExited( MouseEvent e ){
+			String s = "The mouse has left the building";
+			tf.setText( s );
+		}
+	}
+}
