cd8b57246acdd9d9e238c19aa7a30e2da62a2462
liyi1003zcmu <liyi@zcmu.edu.cn>
2021-09-13 13:00:46 +0800

    Delete TestGeneric.java

--- CourseWare/SourceCode/Lec_06_Src/TestGeneric.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class TestGeneric{
-	public static void main( String[] args ){
-		//Comparable c = new Date();
-		Comparable< Date > c = new Date();
-		System.out.println( c.compareTo( "red" ) );
-	}
-}
