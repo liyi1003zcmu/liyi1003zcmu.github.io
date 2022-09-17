69f79485d006a8a5a8ddc1fbc2e667ee4ff014c4
liyi@zcmu.edu.cn <liyi@zcmu.edu.cn>
2022-09-17 09:54:31 -0300

    Create trianglecolor.js

---
+++ js/ch01/trianglecolor.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var gl;
+var points;
+
+window.onload = function init(){
+	var canvas = document.getElementById( "triangle-canvas" );
+	gl = WebGLUtils.setupWebGL( canvas );
+	if( !gl ){
+		alert( "WebGL isn't available" );
+	}
+
+	// Three Vertices
+	var vertices = [
+		-1.0, -1.0, 
+		 1.0, -1.0, 
+		 0.0,  1.0, 
+		 /*0.0, -1.0,
+		 1.0, -1.0,
+		 1.0,  1.0,
+		 0.0, -1.0,
+		 1.0,  1.0,
+		 0.0,  1.0*/
+		 /*-0.5, -0.5,
+		 0.0, 0.5,
+		 0.5, -0.5*/
+	];
+
+	var vertcolors = [
+		1.0, 0.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0
+	];
+	// Configure WebGL
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+	// Load shaders and initialize attribute buffers
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
+
+	// Load the data into the GPU
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( vertices ), gl.STATIC_DRAW );
+
+	// Associate external shader variables with data buffer
+	var aPosition = gl.getAttribLocation( program, "aPosition" );
+	gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( aPosition );
+
+	var cbufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, cbufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( vertcolors ), gl.STATIC_DRAW );
+
+	var aColor = gl.getAttribLocation( program, "aColor" );
+	gl.vertexAttribPointer( aColor, 3, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( aColor );
+
+	render();
+}
+
+function render(){
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	//gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+	gl.drawArrays( gl.TRIANGLES, 0, 3 );
+	//gl.drawArrays( gl.TRIANGLE_FANS, 3, 6 );
+}
