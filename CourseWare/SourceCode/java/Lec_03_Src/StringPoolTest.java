abd32d09983e907c8cbf65912d7bdc7fb4c5e52f
liyi@zcmu.edu.cn <liyi@zcmu.edu.cn>
2024-03-20 21:11:01 +0800

    Update StringPoolTest.java

--- CourseWare/SourceCode/java/Lec_03_Src/StringPoolTest.java
+++ CourseWare/SourceCode/java/Lec_03_Src/StringPoolTest.java
@@ -2,19 +2,24 @@ public class StringPoolTest{
 	public static void main( String[] args ){
 		String s1 = "hello";
 		System.out.println( s1.hashCode() );
+		System.out.println( s1.hashCode() & Integer.MAX_VALUE );
+		
 		System.out.println( "s1="+System.identityHashCode( s1 ) );
 		s1 = "hello2";
 		System.out.println( s1.hashCode() );
+		System.out.println( s1.hashCode() & Integer.MAX_VALUE );
 		System.out.println( "s1="+System.identityHashCode( s1 ) );
+		
 		String s2 = new String( "hello2" );
 		System.out.println( s2.hashCode() );
+		System.out.println( s2.hashCode() & Integer.MAX_VALUE );
 		System.out.println( "s2="+System.identityHashCode( s2 ) );
+		
 		System.out.println( "s1==s2? " + (s1==s2) );
 		System.out.println( "s1.equals(s2)? " + s1.equals( s2 ) );
 		s2 = "hello";
 		System.out.println( s2.hashCode() );
+		System.out.println( s2.hashCode() & Integer.MAX_VALUE );
 		System.out.println( "s2="+System.identityHashCode( s2 ) );
-		
-		
 	}
 }
