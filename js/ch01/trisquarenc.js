996886f9d81960992c95b95a59680297eca5ec1d
liyi@zcmu.edu.cn <liyi@zcmu.edu.cn>
2024-09-09 15:10:41 +0800

    Update trisquarenc.js

--- js/ch01/trisquarenc.js
+++ js/ch01/trisquarenc.js
@@ -5,26 +5,27 @@ var points;
 
 window.onload = function init() {
     var canvas = document.getElementById("trisquare-canvas");
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) {
-        alert("WebGL isn't available");
-    }
+	gl = canvas.getContext("webgl2");
+	if( !gl ){
+		alert( "WebGL isn't available" );
+	}
 
     // Three Vertices
-    var vertices1 = [
+    var vertices1 = new Float32Array([
         -0.5, 0.5,
         -1.0, -0.5,
         0.0, -0.5
-    ];
+    ]);
 
-    var vertices2 = [
+    var vertices2 = new Float32Array([
         0.0, -0.5,
         1.0, -0.5,
         1.0, 0.5,
         1.0, 0.5,
         0.0, 0.5,
         0.0, -0.5
-    ];
+    ]);
+	
     // Configure WebGL
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
@@ -36,7 +37,7 @@ window.onload = function init() {
     // Load the data into the GPU
     var bufferId1 = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId1);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices1), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices1, gl.STATIC_DRAW);
 
     // Associate external shader variables with data buffer
     var vPosition1 = gl.getAttribLocation(program1, "vPosition");
@@ -49,7 +50,7 @@ window.onload = function init() {
 
     var bufferId2 = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId2);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices2), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices2, gl.STATIC_DRAW);
 
     var vPosition2 = gl.getAttribLocation( program2, "vPosition" );
     gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0 );
