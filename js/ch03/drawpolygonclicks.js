016e220b3743c463d7353a93c17a59f7cc8c1cc9
liyi@zcmu.edu.cn <liyi@zcmu.edu.cn>
2024-10-07 21:52:40 +0800

    Update drawpolygonclicks.js

--- js/ch03/drawpolygonclicks.js
+++ js/ch03/drawpolygonclicks.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { vec2, vec4 } = glMatrix;
+
 var canvas;
 var gl;
 
@@ -26,7 +28,7 @@ var start = [0];
 
 function initPolygonCanvas(){
 	canvas = document.getElementById( "ply-canvas" );
-	gl = WebGLUtils.setupWebGL( canvas );
+	gl = canvas.getContext("webgl2");;
 	if( !gl ){
 		alert( "WebGL isn't available" );
 	}
@@ -52,14 +54,14 @@ function initPolygonCanvas(){
 		var rect = canvas.getBoundingClientRect();
 		var cx = event.clientX - rect.left;
 		var cy = event.clientY - rect.top; // offset
-		t = glMatrix.vec2.fromValues( 2 * cx / canvas.width - 1, 2 * ( canvas.height - cy ) / canvas.height - 1 );
+		t = vec2.fromValues( 2 * cx / canvas.width - 1, 2 * ( canvas.height - cy ) / canvas.height - 1 );
 		gl.bufferSubData( gl.ARRAY_BUFFER, 8 * index, new Float32Array( t ) );
 
 		gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
 		//var c = Math.floor(Math.random() * 1024) % 7;
 		var c = cIndex;
 		c = c * 4;
-		t = glMatrix.vec4.fromValues( colors[c], colors[c+1], colors[c+2], colors[c+3] );
+		t = vec4.fromValues( colors[c], colors[c+1], colors[c+2], colors[c+3] );
 		gl.bufferSubData( gl.ARRAY_BUFFER, 16 * index, new Float32Array( t ) );
 		
 		numIndices[numPolygons]++;
