fcdd085e218fbfcce7f140e0b3cc8fda4832ae60
liyi@zcmu.edu.cn <liyi@zcmu.edu.cn>
2026-07-17 23:43:53 +0800

    replace site with Astro teaching portal

--- CourseWare/SourceCode/java/Lec_04_Src/Note.java
+++
@@ -1,5 +0,0 @@
-public class Note{
-	public final static int MIDDLE_C = 1;
-	public final static int MIDDLE_G = 2;
-	public final static int MIDDLE_F = 3;
-}
--- js/ch06/testobj.js
+++
@@ -1,4 +0,0 @@
-function init(){
-    var obj = loadObjFile('http://localhost:8080/teapot.obj');
-    var normals = computeNormals(obj.vertices, obj.faces);
-}
